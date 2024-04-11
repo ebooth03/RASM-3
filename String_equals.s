905458edfa05ea13ae4cd9f55e88bb29d2a7a665
Vikks1 <102274654+Vikks1@users.noreply.github.com>
2024-04-10 21:46:19 -0700

    Update String_equals.s

--- String_equals.s
+++ String_equals.s
@@ -13,37 +13,37 @@
 	.text
 String_equals:
 	// Push registers
-	STP	X19, X20, [SP, #-16]! // Push X19 and X20 to the stack
-	STP	X21, X22, [SP, #-16]! // Push X21 and X22 to the stack
-	STP	X23, X24, [SP, #-16]! // Push X23 and X24 to the stack
-	STP	X25, X26, [SP, #-16]! // Push X25 and X26 to the stack
-	STP	X27, X28, [SP, #-16]! // Push X27 and X28 to the stack
-	STP	X29, X30, [SP, #-16]! // Push X29 and X30 to the stack
+	STP	X19, X20, [SP, #-16]! 	// Push X19 and X20 to the stack
+	STP	X21, X22, [SP, #-16]! 	// Push X21 and X22 to the stack
+	STP	X23, X24, [SP, #-16]! 	// Push X23 and X24 to the stack
+	STP	X25, X26, [SP, #-16]! 	// Push X25 and X26 to the stack
+	STP	X27, X28, [SP, #-16]! 	// Push X27 and X28 to the stack
+	STP	X29, X30, [SP, #-16]! 	// Push X29 and X30 to the stack
 
 	/*===== String_equals =====*/
 
-	MOV	X19, #0					// Initialize LCV (index for strings)
+	MOV	X19, #0			// Initialize LCV (index for strings)
 mainLoop:
-	LDRB	W20, [X0, X19]			// Load a byte from first string
-	LDRB	W21, [X1, X19]			// Load a byte from second string
-	CMP	W20, W21					// Compare W20 to W21
-	BNE	unEqual					// Branch to unEqual if characters are not equal
+	LDRB	W20, [X0, X19]		// Load a byte from first string
+	LDRB	W21, [X1, X19]		// Load a byte from second string
+	CMP	W20, W21		// Compare W20 to W21
+	BNE	unEqual			// Branch to unEqual if characters are not equal
 	CBZ	W20, checkStringEnd	// Branch to checkStringEnd if end of string
 
-	ADD	X19, X19, #1			// Increment pointer
-	B		mainLoop					// Keep looping if characters are same
+	ADD	X19, X19, #1		// Increment pointer
+	B	mainLoop		// Keep looping if characters are same
 
 unEqual:
-	MOV	X0, #0					// Move 0 into X0, meaning false
-	B		end						// Branch to end
+	MOV	X0, #0			// Move 0 into X0, meaning false
+	B	end			// Branch to end
 
 checkStringEnd:
-	LDRB	W21, [X1, X19]			// Load a byte from second string
-	CBZ	W21, equal				// If second string ended as well, than they are the same
-	B		unEqual					// B to unEqual
+	LDRB	W21, [X1, X19]		// Load a byte from second string
+	CBZ	W21, equal		// If second string ended as well, than they are the same
+	B	unEqual			// B to unEqual
 
 equal:
-	MOV	X0, #1					// Move 1 into X0, meaning true
+	MOV	X0, #1			// Move 1 into X0, meaning true
 
 end:
 	/*===== String_equals =====*/
