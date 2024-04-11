aa5fb255267554dec6e949286e48d0dc3f4d61b4
Vikks1 <102274654+Vikks1@users.noreply.github.com>
2024-04-10 21:29:58 -0700

    Update String_charAt.s

--- String_charAt.s
+++ String_charAt.s
@@ -13,15 +13,15 @@
 	.text
 String_charAt:
 	// Push registers
-	STP	X19, X20, [SP, #-16]! // Push X19 and X20 to the stack
-	STP	X21, X22, [SP, #-16]! // Push X21 and X22 to the stack
-	STP	X23, X24, [SP, #-16]! // Push X23 and X24 to the stack
-	STP	X25, X26, [SP, #-16]! // Push X25 and X26 to the stack
-	STP	X27, X28, [SP, #-16]! // Push X27 and X28 to the stack
-	STP	X29, X30, [SP, #-16]! // Push X29 and X30 to the stack
+	STP	X19, X20, [SP, #-16]! 	// Push X19 and X20 to the stack
+	STP	X21, X22, [SP, #-16]! 	// Push X21 and X22 to the stack
+	STP	X23, X24, [SP, #-16]! 	// Push X23 and X24 to the stack
+	STP	X25, X26, [SP, #-16]! 	// Push X25 and X26 to the stack
+	STP	X27, X28, [SP, #-16]! 	// Push X27 and X28 to the stack
+	STP	X29, X30, [SP, #-16]! 	// Push X29 and X30 to the stack
 
 	/*===== String_substring_1 =====*/
-	MOV	X29, X0				// Move string into X29
+	MOV	X29, X0			// Move string into X29
 	LDRB	W0, [X29, X1]		// Load a byte from X29 into W0 at index X1
 
 	/*===== String_substring_1 =====*/
